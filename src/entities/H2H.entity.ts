74601ada862b275e33a2b49b210ca47d66e5c6a2
Yaasiin AYEVA <ayevayaasiin@gmail.com>
2022-12-25 23:07:15 +0000

    Init Project with adding first entity with typeORM config

---
+++ src/entities/H2H.entity.ts
@@ -0,0 +1,24 @@
+import BaseModel from "./base.entity";
+import { Column, Entity } from "typeorm";
+
+@Entity("h2h")
+export class H2H extends BaseModel {
+
+    @Column({ type: "int" })
+    year: number
+
+    @Column({ type: "date" })
+    time: Date
+
+    @Column({ type: "varchar" })
+    home: string
+
+    @Column({ type: "varchar" })
+    away: string
+
+    @Column({ type: "varchar" })
+    homeScore: string
+
+    @Column({ type: "varchar" })
+    awayScore: string
+}
