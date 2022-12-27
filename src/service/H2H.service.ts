08cc2c7ae7351bf7c4f5df53220c377852ca4dab
Yaasiin AYEVA <ayevayaasiin@gmail.com>
2022-12-27 05:33:53 +0000

    Adding XLSX Lib Implementation to read Excel Sheets from server

--- src/service/H2H.service.ts
+++ src/service/H2H.service.ts
@@ -1,6 +1,7 @@
 import { Repository } from "typeorm"
 import { AppDataSource } from "../data-source"
 import { H2H } from "../entity/H2H.entity"
+import * as XLSX from 'xlsx';
 
 export class H2HService {
 
@@ -23,10 +24,31 @@ export class H2HService {
             .andWhere("h2h.time = :time", { time: h2h.time })
             .andWhere("h2h.away = :away", { away: h2h.away })
             .getOne();
-        
+
         if (h2hExist) {
             throw new Error("Unable to create H2H, H2H already exist");
         }
         return await this.repository.save(h2h);
+    }
+
+    public async createH2HFromXLSX(filePath: string): Promise<H2H[]> {
+
+        const workbook = XLSX.readFile(filePath);
+        const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+        const rows = XLSX.utils.sheet_to_json(worksheet, { raw: true });
+        let h2hCreated = [];
+
+        let row: any;
+        for (row of rows) {
+            const h2h = new H2H();
+            h2h.year = row.year;
+            h2h.time = row.time;
+            h2h.home = row.home;
+            h2h.away = row.away;
+            h2h.homeScore = row.homeScore;
+            h2h.awayScore = row.awayScore;
+            h2hCreated.push(await this.createH2H(h2h));
+        }
+        return h2hCreated;
     }
 }
