99e0177f8cd4114b7cb5d35e2e0d9e53abf51232
Yaasiin AYEVA <ayevayaasiin@gmail.com>
2022-12-30 12:54:17 +0000

    Update user services with loadAll, Renaming existing services

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -9,9 +9,9 @@ export default class UserController {
         this.service = new UserService()
     }
 
-    public loadUserHandler = async (req: AuthenticatedUser, res: Response, next: NextFunction) => {
+    public loadHandler = async (req: AuthenticatedUser, res: Response, next: NextFunction) => {
         try {
-            const data = await this.service.loadUser(req.user)
+            const data = await this.service.load(req.user)
             return res.status(200).json({
                 message: 'success',
                 data: data
@@ -21,9 +21,21 @@ export default class UserController {
         }
     }
 
-    public loadUserByIdHandler = async (req: any, res: Response, next: NextFunction) => {
+    public loadAllHandler = async (req: AuthenticatedUser, res: Response, next: NextFunction) => {
         try {
-            const user = await this.service.loadUserById(req.params.id)
+            const data = await this.service.loadAll()
+            return res.status(200).json({
+                message: 'success',
+                data: data
+            })
+        } catch (e) {
+            next(new Error(e.message))
+        }
+    }
+
+    public loadByIdHandler = async (req: any, res: Response, next: NextFunction) => {
+        try {
+            const user = await this.service.loadById(req.params.id)
             return res.status(200).json({
                 message: 'success',
                 data: user
@@ -33,9 +45,9 @@ export default class UserController {
         }
     }
 
-    public deleteUserHandler = async (req: any, res: Response, next: NextFunction) => {
+    public deleteHandler = async (req: any, res: Response, next: NextFunction) => {
         try {
-            const update = await this.service.deleteUser(req.user, req.params.id)
+            const update = await this.service.delete(req.user, req.params.id)
             if (update.affected === 0) {
                 return res.status(404).json({
                     message: 'User not found'
