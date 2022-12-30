a19ec614ead8de1deef4f38d6a7c8e41489c6e69
Yaasiin AYEVA <ayevayaasiin@gmail.com>
2022-12-30 14:19:09 +0000

    Role Police

--- src/middleweare/rolePolice.middleware.ts
+++ src/middleweare/rolePolice.middleware.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Response } from "express";
 import AuthenticatedUser from "../interface/authenticatedUser";
 
-export default async function roleCheckingMiddleware(req: AuthenticatedUser, res: Response, next: NextFunction) {
+export default async function rolePoliceMiddleware(req: AuthenticatedUser, res: Response, next: NextFunction) {
     const role = req.user.role;
     if (role.name !== "Admin") {
         next(new Error("Unauthorized. You must be an admin to perform this action."));
--- src/route/index.route.ts
+++ src/route/index.route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import authenticationMiddleware from "../middleweare/authentication.middleware";
-import roleCheckingMiddleware from "../middleweare/roleChecking.middleware";
+import rolePoliceMiddleware from "../middleweare/rolePolice.middleware";
 import authRoutes from "./auth.route";
 import roleRoutes from "./role.route";
 import userRoutes from "./user.route";
@@ -9,7 +9,7 @@ const apiRouter = Router()
 
 apiRouter.use('/auth', authRoutes);
 apiRouter.use('/users', authenticationMiddleware, userRoutes);
-apiRouter.use('/roles', authenticationMiddleware, roleCheckingMiddleware, roleRoutes);
+apiRouter.use('/roles', authenticationMiddleware, rolePoliceMiddleware, roleRoutes);
 
 export default apiRouter
 
