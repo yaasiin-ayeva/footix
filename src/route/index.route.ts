0e4e8ef303a2556e1e9466981b14368a37e2e00e
Yaasiin AYEVA <ayevayaasiin@gmail.com>
2022-12-30 14:17:19 +0000

    Roles Verification

--- src/route/index.route.ts
+++ src/route/index.route.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import authenticationMiddleware from "../middleweare/authentication.middleware";
+import roleCheckingMiddleware from "../middleweare/roleChecking.middleware";
 import authRoutes from "./auth.route";
+import roleRoutes from "./role.route";
 import userRoutes from "./user.route";
 
 const apiRouter = Router()
 
 apiRouter.use('/auth', authRoutes);
 apiRouter.use('/users', authenticationMiddleware, userRoutes);
+apiRouter.use('/roles', authenticationMiddleware, roleCheckingMiddleware, roleRoutes);
 
 export default apiRouter
 
--- src/route/role.route.ts
+++ src/route/role.route.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import RoleController from "../controller/role.controller";
 
-const userRoutes = Router()
+const roleRoutes = Router()
 const controller = new RoleController()
 
-userRoutes.get('/add', controller.createHandler)
-userRoutes.get('/get-all', controller.loadAllHandler)
-userRoutes.delete('/delete/:id', controller.deleteHandler)
+roleRoutes.get('/add', controller.createHandler)
+roleRoutes.get('/get-all', controller.loadAllHandler)
+roleRoutes.delete('/delete/:id', controller.deleteHandler)
 
-export default userRoutes
+export default roleRoutes
