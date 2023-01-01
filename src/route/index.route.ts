276ba4d541ce5434875c94c26f744f7ea19788f6
Yaasiin AYEVA <ayevayaasiin@gmail.com>
2023-01-01 21:30:36 +0000

    H2H Controller & Routes

--- src/route/index.route.ts
+++ src/route/index.route.ts
@@ -2,15 +2,16 @@ import { Router } from "express";
 import authenticationMiddleware from "../middleweare/authentication.middleware";
 import rolePoliceMiddleware from "../middleweare/rolePolice.middleware";
 import authRoutes from "./auth.route";
+import h2hRoutes from "./h2h.route";
 import roleRoutes from "./role.route";
 import userRoutes from "./user.route";
 
-const apiRouter = Router()
+const apiRouter = Router();
 
 apiRouter.use('/auth', authRoutes);
 apiRouter.use('/users', authenticationMiddleware, userRoutes);
 apiRouter.use('/roles', authenticationMiddleware, rolePoliceMiddleware, roleRoutes);
-// apiRouter.use('/roles', roleRoutes);
+apiRouter.use('/h2h', authenticationMiddleware, rolePoliceMiddleware, h2hRoutes);
 
-export default apiRouter
+export default apiRouter;
 
