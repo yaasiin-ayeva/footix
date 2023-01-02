e958d5902af304e46cef7af4150a4b66bee4a0b0
Yaasiin AYEVA <ayevayaasiin@gmail.com>
2023-01-02 01:32:09 +0000

    Modifying dump data

--- src/dump/role.sql
+++ src/dump/role.sql
@@ -11,12 +11,6 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Database: `footix`
 --
@@ -24,45 +18,9 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `role`
---
-
-CREATE TABLE `role` (
-  `id` int(11) NOT NULL,
-  `createdDatetime` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  `updatedDatetime` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-  `name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
 -- Dumping data for table `role`
 --
 
-INSERT INTO `role` (`id`, `createdDatetime`, `updatedDatetime`, `name`) VALUES
-(1, '2023-01-01 20:33:52.646860', '2023-01-01 20:33:52.646860', 'Admin'),
-(2, '2023-01-01 20:36:26.513957', '2023-01-01 20:36:26.513957', 'User');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `role`
---
-ALTER TABLE `role`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `role`
---
-ALTER TABLE `role`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+INSERT INTO `role` (`name`) VALUES
+('Admin'),
+('User');
